387874ace5c7c4cb1919ab2278af5e42a69b2410
Rafly <rayhansyah817@gmail.com>
2025-02-08 11:42:59 +0700

    Update Dashboard

--- TubesStreamlit.py
+++ TubesStreamlit.py
@@ -2,11 +2,15 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from streamlit_option_menu import option_menu
 
 #Load Data CSV
 def load_data(url) :
     df = pd.read_csv(url)
     return df
+
+df_main = load_data("https://raw.githubusercontent.com/raflyryhnsyh/Tubes-PDSD-Air-Quality/refs/heads/main/Dataset/PRSA_Data_Aotizhongxin_20130301-20170228.csv")
+
 
 with st.sidebar :
     selected = option_menu('Menu',['Dashboard'],
@@ -16,6 +20,7 @@ with st.sidebar :
     
 if (selected == 'Dashboard') :
     st.header(f"Dashboard Analisis Air Quality")
+    df_main
     tab1,tab2 = st.tabs(["Analisis 1", "Analisis 2"])
 
     with tab1 :
